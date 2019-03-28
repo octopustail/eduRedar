9b811ffa70927814ffa9512d562bb947822b6f61
octopustail <494479914@qq.com>
2019-03-28 11:10:18 +0800

    把丑陋的回调地狱改成了promise.all

--- api/routes/stuGpa.js
+++ api/routes/stuGpa.js
@@ -5,56 +5,79 @@ let stuAE = require('../../model/stu_ae')
 let stuType = require('../../model/stu_type')
 function studentGPADataProcess(req, res, next) {
     let stype = req.query.stype || 'good'
-    let list = req.query.list
+    let liststr = req.query.list
     //后台保存一个学生分类的students对象，里面包含各个种类的学生的学号列表
     // let queryID = req.query.stype || null
+   let list =  liststr.split(',')
+
 
     let responseData = { gpa: [], ae: [], records: [] }
-    stuGPA.find({ sid: { $in: list } })
-        .then(result => {
+    // stuGPA.find({ sid: { $in: list } })
+    //     .then(result => {
 
-            // responseData.gpa = {nodes,links};
-            responseData.gpa = { result, stype: stype };
-            stuLib10s.find({ sid: { $in: list } })
-                .then((result) => {
-                    console.log('result2', result)
-                    responseData.records = result
-                    stuAE.find({ sid: { $regex: /^2906/ } })
-                        .then((result) => {
-                            responseData.ae = result
-                            util.responseClient(res, 200, 0, 'success', responseData)
+    //         // responseData.gpa = {nodes,links};
+    //         responseData.gpa = { result, stype: stype };
+    //         util.responseClient(res, 200, 0, 'success', responseData)
 
-                        })
-                })
+    //         // stuLib10s.find({ sid: { $in: list } })
+    //         //     .then((result) => {
+    //         //         console.log('result2', result)
+    //         //         responseData.records = result
+    //         //         stuAE.find({ sid: { $in: list} })
+    //         //             .then((result) => {
+    //         //                 responseData.ae = result
+    //         //                 util.responseClient(res, 200, 0, 'success', responseData)
 
-        })
-        .catch((error) => {
-            console.log(error)
-        })
+    //         //             // })
+    //         //     })
+
+    //     })
+    //     .catch((error) => {
+    //         console.log(error)
+    //     })
     //这么写真的非常丑陋，希望通过promise的方式来进行两次查询异步控制，当两次都查询都返回结果的时候在response，但是没有做到。之后代码优化的点
 
-    // const findstuGPA = new Promise((resolve, reject) => {
-    //     console.log('in promise')
-    //     return stuGPA.find({ sid: { $regex: /^2906/ } })
-    //     // .then((result) => {
-    //     //     console.log('lib', result)
-    //     //     responseData.records = result
-    //     //     resolve()
+    const findstuGPA = new Promise((resolve, reject) => {
+        console.log('in promise1')
+        // resolve('p1')
+        stuGPA.find({ sid: { $in: list } }).then((results)=>{resolve(results)})
+        // stuGPA.find({ sid: { $in: list } }).then((results)=>{resolve(results)}).catch((err)=>{reject(err)})
+        console.log('out promise1')
 
-    //     // })
-    // })
+    })
 
-    // const findstuLib = new Promise((resolve, reject) => {
-    //     console.log('in promise')
-    //     return stuLib10s.find({ sid: { $regex: /^2906/ } })
-    //     // .then((result) => {
-    //     //     console.log('lib', result)
-    //     //     responseData.records = result
-    //     //     resolve()
+    const findstuLib = new Promise((resolve, reject) => {
+        console.log('in promise2')
 
-    //     // })
-    // })
+        // stuLib10s.find({ sid: { $in: list } }).then((results)=>{resolve(results)}).catch((err)=>{reject(err)})
+        stuLib10s.find({ sid: { $in: list } }).then((results)=>{resolve(results)})
+        console.log('out promise1')
 
+        
+    })
+    const findstuAE = new Promise((resolve, reject) => {
+        console.log('in promise3')
+        // resolve('p1')
+
+        // stuAE.find({ sid: { $in: list} }).then((results)=>{resolve(results)}).catch((err)=>{reject(err)})
+        stuAE.find({ sid: { $in: list} }).then((results)=>{resolve(results)})
+        console.log('out promise1')
+        
+    })
+           
+    //results、error都没有打印出来。
+Promise.all([findstuGPA,findstuLib,findstuAE])
+.then((results)=>{
+    responseData.gpa = results[0] 
+    responseData.records = results[1]
+    responseData.ae = results[2]
+    responseData.stype = stype
+    util.responseClient(res, 200, 0, 'success', responseData)
+    console.log(results)
+})
+.catch((err)=>{
+    console.log('err',err)
+})
     // const findstuGPA = stuGPA.find({ sid: { $regex: /^2906/ } })
     // const findstuLib = stuLib10s.find({ sid: { $regex: /^2906/ } })
 
