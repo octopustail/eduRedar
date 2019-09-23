9d40092576e777a71074eed466896dba5ff67969
octopustail <494479914@qq.com>
2019-09-23 21:34:18 +0800

    增加stuList的saga

--- src/saga/indexSaga.js
+++ src/saga/indexSaga.js
@@ -1,8 +1,16 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-01-25 16:58:55
+ * @LastEditTime: 2019-01-25 16:58:55
+ * @LastEditors: your name
+ */
 import {fork} from 'redux-saga/effects'
 
 import {getPersonalRecordFlow} from './personalSaga'
 import {getGeneralGpaRecordFlow} from './generalSaga'
 import {getStudentGroupFlow} from './generalSaga'
+import {getStudentListFlow} from './generalSaga'
 import {getGroupCountFlow} from './groupSaga'
 import {getGroupCountRecords} from './groupSaga'
 import {getGroupStudentsFlow} from './groupSaga'
@@ -11,6 +19,7 @@ export default function* rootSaga(){
     yield fork(getPersonalRecordFlow)
     yield fork(getGeneralGpaRecordFlow)
     yield fork(getStudentGroupFlow)
+    yield fork(getStudentListFlow)
     yield fork(getGroupCountFlow)
     yield fork(getGroupCountRecords)
     yield fork(getGroupStudentsFlow)
