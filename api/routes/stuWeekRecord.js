1aa823348c6a5edf680a995a2649cd183626e3a6
octopustail <494479914@qq.com>
2020-02-24 20:28:09 +0800

    代码整理

--- api/routes/stuWeekRecord.js
+++ api/routes/stuWeekRecord.js
@@ -2,14 +2,13 @@
  * @Description: In User Settings Edit
  * @Author: your name
  * @Date: 2019-03-27 20:45:41
- * @LastEditTime: 2019-11-19 15:27:00
- * @LastEditors: Please set LastEditors
+ * @LastEditTime : 2020-02-24 20:07:11
+ * @LastEditors  : Please set LastEditors
  */
 let util = require('./util')
 let stuGPA = require('../../model/stu_gpa')
 let stuLib10s = require('../../model/stu_lib_10')
 let stuAE = require('../../model/stu_ae')
-let stuType = require('../../model/stu_type')
 let StuRecord  = require('../../model/stu_record')
 const models = require('../../model/stu_counts_09')
 
--- model/stu_type.js
+++
@@ -1,6 +0,0 @@
-let mongoose = require('mongoose')
-let stuTypeSchema = require('../schema/stu_gpa')
-
-
-
-module.exports = mongoose.model('stu_type_09s',stuTypeSchema);
